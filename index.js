245d0ea2865f4042606587d5ea0547fa9fb09187
benpgraham <86138911+benpgraham@users.noreply.github.com>
2021-11-13 12:10:35 +1100

    updated local host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const loaders = require('./loaders');
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // Initialise the app
